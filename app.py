c10dee008d9febe09d2824aa72e85967c3d0357a
Saverio <SaCut@users.noreply.github.com>
2021-04-07 11:25:00 +0100

    added login page with functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -15,11 +15,22 @@ def index():
 def home():
     return render_template("index.html")
 
+@app.route("/login/", methods=["GET", "POST"])
+def login():
+    error = None
+    if request.method == "POST":
+        if request.form["username"] != "admin" or request.form["password"] != "admin":
+            print((type(request.form["username"]), request.form["password"]))
+            error = "Invalid Credentials. Please try again."
+        else:
+            return redirect(url_for("home"))
+    return render_template("login.html", error=error)
+
 @app.route("/welcome/")
 def welcome():
     return "Welcome onboard!"
 
-# create two more toutes, add the functionality for email and password
+# create two more routes, add the functionality for email and password
 
 
 if __name__=='__main__':
